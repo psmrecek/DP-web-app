34fdd76f3ec8057772ecf60e441f9a4ed8e7245c
Peter Smrecek <petersmrecek@gmail.com>
2023-11-21 16:09:17 +0100

    Audit3

--- src/json.js
+++ src/json.js
@@ -11,7 +11,7 @@ export const json = {
           "type": "html",
           "name": "instruction1",
           "state": "expanded",
-          "html": "<iframe width=\"1120\" height=\"630\" src=\"https://www.youtube.com/embed/dOtlQJh5T4w?si=zJwgtkFp3JixN-ou\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>"
+          "html": "<iframe width=\"840\" height=\"473\" src=\"https://www.youtube.com/embed/dOtlQJh5T4w?si=zJwgtkFp3JixN-ou\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>"
         }
       ]
     },
